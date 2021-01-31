041b72449f76deeecf19692f4d14aaf02275b86d
Quant Girl <quantgirl.uk@gmail.com>
2021-01-31 21:40:38 +0000

    Desing Patterns Book
    Design Paterns Book Chapter 1

---
+++ Design Patterns /Ch01. Simple MC/PayOff1.cpp
@@ -0,0 +1,12 @@
+//
+//  PayOff1.cpp
+//  Sandbox-Cpp
+//
+//  Created by Dialid Santiago on 31/01/2021.
+//
+
+#include "PayOff1.hpp"
+#include <algorithm>
+using namespace std;
+
+
---
+++ Design Patterns /Ch01. Simple MC/Random1.hpp
@@ -0,0 +1,22 @@
+//
+//  Random1.hpp
+//  Sandbox-Cpp
+//
+//  Created by Dialid Santiago on 31/01/2021.
+//
+
+#ifndef Random1_hpp
+#define Random1_hpp
+
+#include <stdio.h>
+#ifndef Random1_H
+#define Random1_H
+
+double GetOneGaussianBySummation();
+double GetOneGaussianByBoxMuller();
+
+#endif
+
+
+
+#endif /* Random1_hpp */
