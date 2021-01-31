ef70df4d0a266b7a352436700517b19596ec82db
Quant Girl <quantgirl.uk@gmail.com>
2021-01-31 00:26:10 +0000

    Initial Commit

---
+++ Sandbox-Cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sandbox-Cpp
+//
+//  Created by Dialid Santiago on 31/01/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
