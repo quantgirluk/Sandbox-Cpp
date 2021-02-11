b1ae4ef0c16f786e721ef192bcf4ea77265930be
Quant Girl <quantgirl.uk@gmail.com>
2021-02-11 22:44:38 +0000

    Updated main file.

--- Design Patterns/Chapter_01_Simple_MC/Exercises/main.cpp
+++ Design Patterns/Chapter_01_Simple_MC/Exercises/main.cpp
@@ -10,6 +10,7 @@
 #include "PayOff.hpp"
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -53,43 +54,33 @@ int main()
     unsigned long NumberOfPath;
     
     //read in parameters
-    cout << "\nEnter Expiry T: \n";
+    cout << "\nEnter Expiry T:";
     cin >> Expiry;
     
-    cout << "\nEnter Strike K:\n";
+    cout << "\nEnter Strike K:";
     cin >> Strike;
     
-    cout << "\nEnter Spot S0:\n";
+    cout << "\nEnter Spot S0:";
     cin >> Spot;
     
-    cout << "\nEnter Volatility:\n";
+    cout << "\nEnter Volatility:";
     cin >> Vol;
     
-    cout << "\nEnter Interest Rate r:\n";
+    cout << "\nEnter Interest Rate r:";
     cin >> r;
     
-    cout << "\nNumber of MC paths:\n";
+    cout << "\nNumber of MC paths:";
     cin >> NumberOfPath;
     
     PayOff callPayOff(Strike, PayOff::call);
     PayOff putPayOff(Strike, PayOff::put);
     
-    double resultCall = SimpleMonteCarlo(callPayOff,
-                                          Expiry,
-                                      Spot,
-                                      Vol,
-                                      r,
-                                      NumberOfPath);
-    double resultPut = SimpleMonteCarlo(putPayOff,
-                                          Expiry,
-                                      Spot,
-                                      Vol,
-                                      r,
-                                      NumberOfPath);
+    double resultCall = SimpleMonteCarlo(callPayOff,Expiry, Spot, Vol, r, NumberOfPath);
+    double resultPut = SimpleMonteCarlo(putPayOff,Expiry, Spot, Vol, r, NumberOfPath);
 
     
-    cout << "The Prices are:\n"<< resultCall << " for the Call\n";
-    cout << resultPut<< " for the Put\n";
+    cout << "The Prices are:\n"<< setprecision(3) <<resultCall << " for the Call\n";
+    cout << setprecision(3) << resultPut<< " for the Put\n";
     double tmp;
     cin >>tmp;
 
